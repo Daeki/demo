e89dff78174e83d2e6fd815b22182e1dfaf8724d
Daeki <kimdaeki@gmail.com>
2023-04-04 09:27:46 +0900

    3

---
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -0,0 +1,19 @@
+package com.example.demo.home;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@Value("test")
+	private String msg;
+	
+	@GetMapping("/")
+	public String home() {
+		System.out.println(msg);
+		return "home";
+	}
+
+}
