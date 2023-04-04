fbd94de4d3d8b3099dc4b0f8bc4b011200a440ee
Daeki <kimdaeki@gmail.com>
2023-04-04 16:15:36 +0900

    1

--- src/main/java/com/example/demo/home/HomeController.java
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
-	@Value("test")
+	@Value("${test}")
 	private String msg;
 	
 	@GetMapping("/")
