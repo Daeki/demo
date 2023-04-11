d0dc792846086335644ebad5718d296de0515f62
Daeki <kimdaeki@gmail.com>
2023-04-11 11:50:31 +0900

    1

--- src/main/java/com/example/demo/home/HomeController.java
+++ src/main/java/com/example/demo/home/HomeController.java
@@ -9,24 +9,14 @@ public class HomeController {
 	
 	@Value("${name}")
 	private String name;
-	
-	@Value("${age}")
-	private String age;
-	
-	@Value("${email}")
-	private String email;
-	
-	@Value("${phone}")
-	private String phone;
+
 	
 	
 	
 	@GetMapping("/")
 	public String home() {
 		System.out.println(name);
-		System.out.println(age);
-		System.out.println(email);
-		System.out.println(phone);
+
 		return "homeupdate";
 	}
 
